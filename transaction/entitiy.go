a4f3b3154da8719862d47daa7d93da6489af10a0
Muhammad Andhika <mandhikarizq@gmail.com>
2023-02-28 20:17:24 +0700

    Preperation Transaction

---
+++ transaction/entitiy.go
@@ -0,0 +1,15 @@
+package transaction
+
+import "time"
+
+//Reperesentasi dari tabel transaction di database
+type Transaction struct {
+	ID         int
+	CampaignID int
+	UserID     int
+	Amount     int
+	Status     string
+	Code       string
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
